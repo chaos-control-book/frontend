e676ef28510d92010cafd3db579c5d5f35a15df7
Aleksey Shelementev <rushelex@gmail.com>
2021-11-09 13:35:24 +0500

    feat: adds base styles structure

---
+++ src/pages/_document.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from 'react';
+
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+
+// eslint-disable-next-line import/no-default-export
+export default class CustomDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="ru">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
