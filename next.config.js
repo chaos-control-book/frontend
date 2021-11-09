c6c07b69fc9edad18ddb280fff4f9249c5a3b109
Aleksey Shelementev <rushelex@gmail.com>
2021-11-09 13:31:03 +0500

    chore: adds sass и bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfiguration = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/shared/styles/3-helpers')],
+  },
+};
+
+module.exports = withPlugins([[withBundleAnalyzer]], nextConfiguration);
