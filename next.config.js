fb4ca9d4ad1d3de967f33b6b4de8e7dfbab0f643
Aleksey Shelementev <rushelex@gmail.com>
2021-11-16 19:12:05 +0500

    feat: adds pwa
    Signed-off-by: Aleksey Shelementev <rushelex@gmail.com>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 /** @type {import('next').NextConfig} */
 const nextConfiguration = {
@@ -13,6 +15,12 @@ const nextConfiguration = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/shared/styles/3-helpers')],
   },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+  },
 };
 
-module.exports = withPlugins([[withBundleAnalyzer]], nextConfiguration);
+module.exports = withPlugins([withBundleAnalyzer, withPWA], nextConfiguration);
